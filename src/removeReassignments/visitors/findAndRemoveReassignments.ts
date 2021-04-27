b6d8d9388fea8b974fe7f29009838e9a41d141bf
Patryk Heljasinski <heljasinskip@gmail.com>
2021-04-27 18:59:05 +0200

    Improved reassignments (BlockStatement visitor)

--- src/removeReassignments/visitors/findAndRemoveReassignments.ts
+++ src/removeReassignments/visitors/findAndRemoveReassignments.ts
@@ -1,6 +1,7 @@
 import { NodePath, Visitor } from '@babel/traverse';
 import {
   ArrowFunctionExpression,
+  BlockStatement,
   ClassMethod,
   FunctionDeclaration,
   FunctionExpression,
@@ -37,73 +38,49 @@ const pushVariable = (
   variable.paths.push(child);
 };
 
-const handler = (
-  path:
-    | NodePath<FunctionDeclaration>
-    | NodePath<FunctionExpression>
-    | NodePath<ClassMethod>
-    | NodePath<ArrowFunctionExpression>
-    | NodePath<ObjectMethod>,
-) => {
-  if (!path.node.start) return;
-  const program = path.find((p) => p.isProgram());
-  if (!program) return;
-
-  const body = path.get('body') as NodePath;
-  if (!body.isBlockStatement()) return;
-  const variables: ReassignedVariable[] = [];
-  const blockChilds = body.get('body') as NodePath[];
-  for (const child of blockChilds) {
-    if (!child.isVariableDeclaration() && !child.isExpressionStatement()) {
-      break;
-    }
-    if (child.isVariableDeclaration()) {
-      const [declaration] = child.get('declarations') as NodePath[];
-      if (!declaration.isVariableDeclarator()) continue;
-      const id = declaration.node.id;
-      if (!isIdentifier(id)) continue;
-      const init = declaration.node.init;
-      if (!isStringLiteral(init) && !isNumericLiteral(init)) continue;
-      pushVariable(variables, child, id.name, init);
-    } else {
-      const expression = child.get('expression') as NodePath;
-      if (!expression.isAssignmentExpression({ operator: '=' })) continue;
-      const id = expression.node.left;
-      if (!isIdentifier(id)) continue;
-      const init = expression.node.right;
-      if (!isStringLiteral(init) && !isNumericLiteral(init)) continue;
-      pushVariable(variables, child, id.name, init);
-    }
-  }
-  variables.forEach((variable) => {
-    const [firstPath, ...otherPaths] = variable.paths;
-    otherPaths.forEach((p) => p.remove());
-    const init =
-      typeof variable.value === 'string'
-        ? stringLiteral(variable.value)
-        : numericLiteral(variable.value);
-    firstPath.replaceWith(
-      variableDeclaration('var', [
-        variableDeclarator(identifier(variable.name), init),
-      ]),
-    );
-  });
-};
 
 export const FIND_AND_REMOVE_REASSIGNMENTS: Visitor = {
-  FunctionDeclaration: function (path: NodePath<FunctionDeclaration>) {
-    handler(path);
-  },
-  FunctionExpression: function (path: NodePath<FunctionExpression>) {
-    handler(path);
-  },
-  ArrowFunctionExpression: function (path: NodePath<ArrowFunctionExpression>) {
-    handler(path);
-  },
-  ObjectMethod: function (path: NodePath<ObjectMethod>) {
-    handler(path);
-  },
-  ClassMethod: function (path: NodePath<ClassMethod>) {
-    handler(path);
+  BlockStatement: function (path: NodePath<BlockStatement>) {
+    if (!path.node.start) return;
+    const program = path.find((p) => p.isProgram());
+    if (!program) return;
+
+    const variables: ReassignedVariable[] = [];
+    const blockChilds = path.get('body') as NodePath[];
+    for (const child of blockChilds) {
+      if (!child.isVariableDeclaration() && !child.isExpressionStatement()) {
+        continue;
+      }
+      if (child.isVariableDeclaration()) {
+        const [declaration] = child.get('declarations') as NodePath[];
+        if (!declaration.isVariableDeclarator()) continue;
+        const id = declaration.node.id;
+        if (!isIdentifier(id)) continue;
+        const init = declaration.node.init;
+        if (!isStringLiteral(init) && !isNumericLiteral(init)) continue;
+        pushVariable(variables, child, id.name, init);
+      } else {
+        const expression = child.get('expression') as NodePath;
+        if (!expression.isAssignmentExpression({ operator: '=' })) continue;
+        const id = expression.node.left;
+        if (!isIdentifier(id)) continue;
+        const init = expression.node.right;
+        if (!isStringLiteral(init) && !isNumericLiteral(init)) continue;
+        pushVariable(variables, child, id.name, init);
+      }
+    }
+    variables.forEach((variable) => {
+      const [firstPath, ...otherPaths] = variable.paths;
+      otherPaths.forEach((p) => p.remove());
+      const init =
+        typeof variable.value === 'string'
+          ? stringLiteral(variable.value)
+          : numericLiteral(variable.value);
+      firstPath.replaceWith(
+        variableDeclaration('var', [
+          variableDeclarator(identifier(variable.name), init),
+        ]),
+      );
+    });
   },
 };
