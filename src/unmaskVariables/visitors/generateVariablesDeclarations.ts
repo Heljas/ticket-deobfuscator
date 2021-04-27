1eb7099d52979de1378fa4694a222b5b76bdfa41
Patryk Heljasinski <heljasinskip@gmail.com>
2021-04-27 21:27:27 +0200

    Variables masking for of/in & undefined fix

--- src/unmaskVariables/visitors/generateVariablesDeclarations.ts
+++ src/unmaskVariables/visitors/generateVariablesDeclarations.ts
@@ -1,11 +1,19 @@
 import { NodePath, Scope, Visitor } from '@babel/traverse';
-import { identifier, MemberExpression, variableDeclaration, variableDeclarator } from '@babel/types';
+import {
+  identifier,
+  MemberExpression,
+  variableDeclaration,
+  variableDeclarator,
+} from '@babel/types';
 import { getMaskedVariableIndex } from '../getMaskedVariableIndex';
 import { generateIdentifier } from '../generateIdentifier';
 import { VariablesMaskingState } from '../types/VariablesMaskingState';
 
 export const GENERATE_VARIABLES_DECLARATIONS: Visitor = {
-  MemberExpression: function (path: NodePath<MemberExpression>, state: VariablesMaskingState) {
+  MemberExpression: function (
+    path: NodePath<MemberExpression>,
+    state: VariablesMaskingState,
+  ) {
     const index = getMaskedVariableIndex(path, state);
     if (index === -1) return;
     const maskedVariable = state.maskedVariables.find((m) => m.index === index);
@@ -14,31 +22,66 @@ export const GENERATE_VARIABLES_DECLARATIONS: Visitor = {
 
     //Make sure we are on the left side of the assignment expression
     if (
-      !parent.isAssignmentExpression({ operator: '=' }) ||
-      path.key !== 'left'
-    )
-      return;
-
-    const name = generateIdentifier(state.globalScope);
-    state.maskedVariables.push({
-      name,
-      index,
-    });
-    const initialValue = parent.node.right;
-    const id = identifier(name);
-    const declarator = variableDeclarator(id, initialValue);
-    const declaration = variableDeclaration('var', [declarator]);
-
-    /*
-      ExpressionStatement/ForStatement
-        AssignmentExpression
-          MemberExpression <- we are here
-    */
-    if (parent.parentPath.isExpressionStatement()) {
-      parent.parentPath.replaceWith(declaration);
-    } else {
-      //parent.parentPath is ForStatement, we need to replace AssignmentExpression
-      parent.replaceWith(declaration);
+      parent.isAssignmentExpression({ operator: '=' }) &&
+      path.key === 'left'
+    ) {
+      handleAssignmentExpression(path, state, index);
+    } else if (parent.isForOfStatement() || parent.isForInStatement()) {
+      handleForLoops(path, state, index);
     }
   },
 };
+
+function handleAssignmentExpression(
+  path: NodePath<MemberExpression>,
+  state: VariablesMaskingState,
+  index: number,
+) {
+  const parent = path.parentPath;
+  if (!parent.isAssignmentExpression()) {
+    return;
+  }
+  const outerPath = parent.parentPath;
+  /*
+      ExpressionStatement/ForStatement (outerPath)
+        AssignmentExpression
+          MemberExpression <- we are here
+  */
+
+  if (!outerPath.isExpressionStatement() && !outerPath.isForStatement()) {
+    return;
+  }
+
+  const name = generateIdentifier(state.globalScope);
+  state.maskedVariables.push({
+    name,
+    index,
+  });
+  const initialValue = parent.node.right;
+  const id = identifier(name);
+  const declarator = variableDeclarator(id, initialValue);
+  const declaration = variableDeclaration('var', [declarator]);
+
+  if (outerPath.isExpressionStatement()) {
+    outerPath.replaceWith(declaration);
+  } else {
+    //outerPath is ForStatement, we only need to replace AssignmentExpression
+    parent.replaceWith(declaration);
+  }
+}
+
+function handleForLoops(
+  path: NodePath<MemberExpression>,
+  state: VariablesMaskingState,
+  index: number,
+) {
+  const name = generateIdentifier(state.globalScope);
+  state.maskedVariables.push({
+    name,
+    index,
+  });
+  const id = identifier(name);
+  const declarator = variableDeclarator(id);
+  const declaration = variableDeclaration('const', [declarator]);
+  path.replaceWith(declaration);
+}
--- src/unmaskVariables/visitors/replaceWithVariables.ts
+++ src/unmaskVariables/visitors/replaceWithVariables.ts
@@ -1,5 +1,6 @@
 import { NodePath, Visitor } from '@babel/traverse';
 import { identifier, MemberExpression } from '@babel/types';
+import { generateIdentifier } from '../generateIdentifier';
 import { getMaskedVariableIndex } from '../getMaskedVariableIndex';
 import { VariablesMaskingState } from '../types/VariablesMaskingState';
 
@@ -9,9 +10,18 @@ export const REPLACE_WITH_VARIABLES: Visitor = {
     state: VariablesMaskingState,
   ) {
     const index = getMaskedVariableIndex(path, state);
-    if (index === -1) return;
+    if (index < 1) return;
     const maskedVariable = state.maskedVariables.find((m) => m.index === index);
-    if (!maskedVariable) return;
+    if (!maskedVariable) {
+      if (path.key !== 'object') {
+        path.replaceWith(identifier('undefined'));
+      } else {
+        console.error(
+          `Couldn't replace ${path.toString()} with undefined - key is object. Something went wrong earlier!`,
+        );
+      }
+      return;
+    }
     path.replaceWith(identifier(maskedVariable.name));
   },
 };
